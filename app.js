8334211284243e8cec4d1d0d0cf0b060e32ab48a
Mario Pareja <pareja.mario@gmail.com>
2013-03-04 08:46:13 -0500

    Fix capitalization of people's names, remove Display Name and Notes Image fields.

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 var decsv = require('./decsv');
 var fs = require('fs');
+var lingo = require('lingo');
 var map = require('map-stream');
 
 var startAtLine = process.argv[2] || 0;
 var filter = process.argv[3] ? new RegExp(process.argv[3]) : null;
 
-var stream = decsv('data/contacts_20121230.csc', startAtLine, filter);
+var stream = decsv('data/contacts_filtered.csv', startAtLine, filter);
 var noimage = stream.pipe(map(function (fields, callback) {
-  fields.splice(59, 1);
+  for (var i = 0; i < 13; i++) {
+    // Capitalize peoples names
+    if (fields[i]) {
+      fields[i] = lingo.capitalize(fields[i].toLocaleLowerCase(), true);
+    }
+  }
+  // remove Display Name field
+  fields.splice(7, 1);
+  // remove the notes image field
+  fields.splice(59 - 1, 1);
   callback(null, fields);
 }));
 
 var csved = noimage.pipe(map(function (fields, callback) {
+  var chars = [','];
   var s = fields.map(function (f) {
-    return f.indexOf(',') >= 0 ? '"' + f.replace('"', '\\"') + '"' : f;
+    var needsQuote = chars.some(function (c) { return f.indexOf(c); })
+    return needsQuote ? '"' + f.replace('"', '\\"') + '"' : f;
   }).join(',');
   callback(null, s + '\r\n');
 }));
 csved.pipe(fs.createWriteStream('data/output1.csv'));
-csved.pipe(process.stdout);
+//csved.pipe(process.stdout);
 
 // stream.on('counts', function (counts) { console.log(counts); });
 
--- decsv.js
+++ decsv.js
@@ -1,47 +1,46 @@
+var decsv = require('decsv');
 var fs = require('fs');
 var map = require('map-stream');
 var split = require('split');
 
 module.exports = function (filename, startAtLine, filter) {
+  var binaryRegex = /^(0x[0-9a-fA-F][0-9a-fA-F],?)+$/;
   var rs = fs.createReadStream(filename);
   rs.setEncoding('utf16le');
-  var lineNumber = 0;
 
-  if (startAtLine > 1) {
-  //  console.log('Starting at line: ' + startAtLine);
-  } else {
-    startAtLine = 0;
-  }
-
-  var rowStream = rs.pipe(split(/\r?\n/)).pipe(map(function (line, callback) {
-    lineNumber++;
-    // skip header
-    if (lineNumber < startAtLine) {
-      return callback();
-    } else if (filter && !filter.test(line)) {
-      return callback();
+  var filteredRows = (function () {
+    var lineIndex = 0;
+    var startIndex = startAtLine - 1;
+    if (startIndex > 0) {
+      process.stderr.write('Starting at line: ' + startAtLine + '\n');
+    } else {
+      startIndex = 0;
     }
 
-    var fields = line.split(';').map(function (field) {
-      return field.substring(1, field.length - 1);
-    });
-    callback(null, fields);
-  }));
+    return rs.pipe(decsv(';')).pipe(map(function (fields, callback) {
+      if (lineIndex++ < startIndex) {
+        return callback();
+      } else if (filter && !fields.some(filter.test.bind(filter))) {
+        return callback();
+      }
+      callback(null, fields);
+    }));
+  }());
 
   var counts = {};
-  rowStream.on('data', function (fields) {
+  filteredRows.on('data', function (fields) {
     for (var i = 0; i < fields.length; i++) {
-      if (/0x[0-9a-bA-Z][0-9a-bA-Z]/.test(fields[i])) {
+      if (binaryRegex.test(fields[i])) {
         counts[i] = counts[i] ? counts[i] + 1 : 1;
       }
     }
   });
 
-  var unicoded = rowStream.pipe(map(function (fields, callback) {
+  var unicoded = filteredRows.pipe(map(function (fields, callback) {
     // find encoded unicode fields and decode them
     var f = fields.slice(0); // clone it
     for (var i = 0; i < fields.length; i++) {
-      if (/0x[0-9a-bA-Z][0-9a-bA-Z],0x[0-9a-bA-Z][0-9a-bA-Z]/.test(f[i])) {
+      if (binaryRegex.test(f[i])) {
         var octects = breakIntoOctects(f[i]);
         var buffer = new Buffer(octects)
         f[i] = buffer.toString('utf16le');
